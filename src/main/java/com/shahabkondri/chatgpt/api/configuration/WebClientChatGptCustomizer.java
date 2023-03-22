00bea6494aed35590764bc6e8fb7c2ac1e3aa48e
Shahab Kondri <shahab.kondri@gmail.com>
2023-03-22 01:29:37 -0400

    handle special cases in ChatGpt response stream
    This commit introduces the ChatGptResponseFilterFunction to handle
    special cases in the ChatGpt response stream, such as removing or
    replacing unwanted content like the "[DONE]" message. The filter
    function prevents deserialization issues when parsing the response
    stream.

--- src/main/java/com/shahabkondri/chatgpt/api/configuration/WebClientChatGptCustomizer.java
+++ src/main/java/com/shahabkondri/chatgpt/api/configuration/WebClientChatGptCustomizer.java
@@ -3,13 +3,25 @@ package com.shahabkondri.chatgpt.api.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.reactive.function.client.WebClientCustomizer;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.*;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.nio.charset.StandardCharsets;
 
 /**
+ * {@link WebClientCustomizer} that customizes the {@link WebClient} for interacting with
+ * the ChatGPT API.
+ * <p>
+ * This class customizes the {@link WebClient.Builder} instance by setting the base URL,
+ * content type, and authorization header. It uses {@link OpenApiProperties} to retrieve
+ * the necessary configuration properties.
+ *
  * @author Shahab Kondri
  */
 @Configuration
@@ -28,9 +40,43 @@ public class WebClientChatGptCustomizer implements WebClientCustomizer {
 
 		DefaultUriBuilderFactory builderFactory = new DefaultUriBuilderFactory(uriBuilder);
 
-		webClientBuilder.uriBuilderFactory(builderFactory)
-				.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON.toString())
+		webClientBuilder.uriBuilderFactory(builderFactory).filter(new ChatGptResponseFilterFunction())
+				.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
 				.defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer " + properties.getApiKey());
+	}
+
+	/**
+	 * An implementation of the {@link ExchangeFilterFunction} that filters the body of
+	 * the {@link ClientResponse} to handle special cases in the response stream, such as
+	 * removing or replacing unwanted content.
+	 *
+	 * <p>
+	 * When the response content contains "[DONE]", an empty {@link DataBuffer} is
+	 * returned instead. This prevents deserialization issues when parsing the response
+	 * stream.
+	 */
+	private final static class ChatGptResponseFilterFunction implements ExchangeFilterFunction {
+
+		@Override
+		public Mono<ClientResponse> filter(ClientRequest request, ExchangeFunction next) {
+			return next.exchange(request).flatMap(response -> {
+				if (response.statusCode().is2xxSuccessful()) {
+					Flux<DataBuffer> filteredBody = response.bodyToFlux(DataBuffer.class).filter(buffer -> {
+						String content = buffer.toString(StandardCharsets.UTF_8);
+						return !content.contains("data: [DONE]");
+					});
+					// Create a new ClientResponse instead of mutating the existing one
+					ClientResponse.Builder builder = ClientResponse.create(response.statusCode())
+							.headers(headers -> headers.addAll(response.headers().asHttpHeaders()))
+							.cookies(cookies -> cookies.putAll(response.cookies()));
+					return Mono.just(builder.body(filteredBody).build());
+				}
+				else {
+					return Mono.just(response);
+				}
+			});
+		}
+
 	}
 
 }
